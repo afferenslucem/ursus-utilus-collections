01d16c0eb8ca26234f06df43438d49a0151a3df0
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-10 09:58:28 +0500

    added tests

--- tests/utils/comparer.spec.ts
+++ tests/utils/comparer.spec.ts
@@ -29,4 +29,21 @@ describe('Comparer', function () {
         assert.equal(-1, comparer.compare([1, 2], [2, 1]));
         assert.equal(-1, comparer.compare([1, 1], [1, 2]));
     });
+    it('should compare with override func', () => {
+        const comparer = new Comparer([
+            {
+                mapping: item => item[0],
+                compare: (first, second) => second - first
+            },
+            {
+                mapping: item => item[1]
+            }
+        ], defaultCompare);
+
+        assert.equal(-1, comparer.compare([2, 1], [1, 1]));
+        assert.equal(1, comparer.compare([1, 2], [1, 1]));
+        assert.equal(0, comparer.compare([1, 1], [1, 1]));
+        assert.equal(1, comparer.compare([1, 2], [2, 1]));
+        assert.equal(-1, comparer.compare([1, 1], [1, 2]));
+    });
 });
