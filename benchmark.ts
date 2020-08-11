1187943fd90028dbf023e72cd5c9384be10371bb
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-11 12:58:59 +0500

    added grouping

--- benchmark.ts
+++ benchmark.ts
@@ -24,13 +24,23 @@ function getArrayDesc(count: number) {
     return array;
 }
 
+function getArrayForGrouping(count: number, disp: number) {
+    const array = [];
+
+    for(let i = 0; i < count; i++) {
+        array.push([i % disp, i]);
+    }
+
+    return array;
+}
+
 function onCycle(event: Benchmark.Event) {console.log(String(event.target))}
 
-function suite(name: string, count = 1000000): Benchmark.Suite {
+function suite(name: string, count = 1000000, arr?: any[]): Benchmark.Suite {
     return new Benchmark.Suite('Filter for 1000000', {
-        onCycle: onCycle,   
+        onCycle: onCycle,
         onStart: function () {
-            array = getArray(count);
+            array = arr != null ? arr : getArray(count);
             console.log(`${name}:`)
         },
         onComplete: function() {
@@ -91,6 +101,54 @@ suite('Filter for 1000000')
     });
 
 suite('Filter for 1000', 1000)
+    .add('Where', function () {
+        return _(array).where(item => (item % 2) == 0)
+        .toArray();
+    })
+    .add('Native filter', function () {
+        return array.filter(item => (item % 2) == 0);
+    })
+    .add('Double where', function () {
+        return _(array).where(item => (item % 2) == 0)
+        .where(item => (item % 3) == 0)
+        .toArray();
+    })
+    .add('Double native filter', function () {
+        return array.filter(item => (item % 2) == 0)
+        .filter(item => (item % 3) == 0);
+    })
+    .add('10 where', function () {
+        return _(array)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .where(item => (item % 2) == 0)
+        .toArray();
+    })
+    .add('10 native filter', function () {
+        return array
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+        .filter(item => (item % 2) == 0)
+    })
+    .run({
+        async: false
+    });
+
+suite('Filter for 10', 10)
     .add('Where', function () {
         return _(array).where(item => (item % 2) == 0)
         .toArray();
@@ -240,51 +298,175 @@ suite('Select for 1000', 1000)
         async: false
     });
 
+suite('Select for 10', 10)
+    .add('Select', function () {
+        return _(array)
+        .select(item => item + 1)
+        .toArray();
+    })
+    .add('Native map', function () {
+        return array.map(item => item + 1)
+    })
+    .add('Double select', function () {
+        return _(array)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .toArray();
+    })
+    .add('Double native map', function () {
+        return array
+        .map(item => item + 1)
+        .map(item => item + 1);
+    })
+    .add('10 select', function () {
+        return _(array)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .select(item => item + 1)
+        .toArray();
+    })
+    .add('10 native map', function () {
+        return array
+        .map(item => item + 1)
+        .map(item => item + 1)
+        .map(item => item + 1)
+        .map(item => item + 1)
+        .map(item => item + 1)
+        .map(item => item + 1)
+        .map(item => item + 1)
+        .map(item => item + 1)
+        .map(item => item + 1)
+        .map(item => item + 1)
+    })
+    .run({
+        async: false
+    });
 
-suite('Sort for 1000000', 1000000)
+let sortingArray = null;
+
+const sortOpt: Benchmark.Options = {
+    onStart: () => sortingArray = Array.from(array)
+}
+
+suite('Sort for 1000000', 0, getArrayDesc(1000000))
     .add('Sort', function () {
         return _(array)
         .sort()
         .toArray();
-    })
+    }, sortOpt)
     .add('Native sort', function () {
         return array.sort()
-    })
+    }, sortOpt)
     .add('SortBy', function () {
         return _(array)
         .sortBy(item => item.toString())
         .toArray();
-    })
+    }, sortOpt)
     .add('Sort after map', function () {
         return array
         .map(item => item.toString())
         .sort();
-    })
+    }, sortOpt)
     .run({
         async: false
     });
 
 
 
-suite('Sort for 1000', 1000)
+suite('Sort for 1000', 0, getArrayDesc(1000))
     .add('Sort', function () {
         return _(array)
         .sort()
         .toArray();
-    })
+    }, sortOpt)
     .add('Native sort', function () {
         return array.sort()
-    })
+    }, sortOpt)
     .add('SortBy', function () {
         return _(array)
         .sortBy(item => item.toString())
         .toArray();
-    })
+    }, sortOpt)
     .add('Sort after map', function () {
         return array
         .map(item => item.toString())
         .sort();
+    }, sortOpt)
+    .run({
+        async: false
+    });
+
+    suite('Sort for 10', 0, getArrayDesc(10))
+    .add('Sort', function () {
+        return _(array)
+        .sort()
+        .toArray();
+    }, sortOpt)
+    .add('Native sort', function () {
+        return array.sort()
+    }, sortOpt)
+    .add('SortBy', function () {
+        return _(array)
+        .sortBy(item => item.toString())
+        .toArray();
+    }, sortOpt)
+    .add('Sort after map', function () {
+        return array
+        .map(item => item.toString())
+        .sort();
+    }, sortOpt)
+    .run({
+        async: false
+    });
+
+suite('Group for 1000000 with desp 25', 0, getArrayForGrouping(1000000, 25))
+    .add('GroupBy', function () {
+        return _(array as any[])
+        .groupBy(item => item[0])
+        .toArray();
     })
+    .add('GroupBy with map', function () {
+        return _(array as any[])
+        .groupBy(item => item[0], group => group.first())
+        .toArray();
+    })
+    .run({
+        async: false
+    });
+
+suite('Group for 1000 with desp 15', 0, getArrayForGrouping(1000, 15))
+    .add('GroupBy', function () {
+        return _(array as any[])
+        .groupBy(item => item[0])
+        .toArray();
+    })
+    .add('GroupBy with map', function () {
+        return _(array as any[])
+        .groupBy(item => item[0], group => group.first())
+        .toArray();
+    })
+    .run({
+        async: false
+    });
+
+suite('Group for 10 with desp 3', 0, getArrayForGrouping(10, 3))
+    .add('GroupBy', function () {
+        return _(array as any[])
+        .groupBy(item => item[0])
+        .toArray();
+    }, sortOpt)
+    .add('GroupBy with map', function () {
+        return _(array as any[])
+        .groupBy(item => item[0], group => group.first())
+        .toArray();
+    }, sortOpt)
     .run({
         async: false
     });
