1a02485b314c6c746c446f98dad8614d51a3cd40
Alex Shakirov <a.shakirov@infinnity.ru>
2020-08-17 15:18:20 +0500

    fixed build

--- src/aggregators/count-aggregator.ts
+++ src/aggregators/count-aggregator.ts
@@ -1,5 +1,6 @@
 import { ICollection } from "../interfaces/i-collection";
 import { Aggregator } from "./aggregator";
+import { FilterCondition } from "../commands/delegates";
 
 export class CountAggregator<T> extends Aggregator<number> {
     public constructor(private collection: ICollection<T>, private condition?: FilterCondition<T>) {
