86695e4e59d6fb423f6441511cec09104ff560ca
ASH <alexshakirov74@gmail.com>
2020-09-13 09:23:10 +0500

    task: added tests for dec

--- src/utils/abstract-equlity-comparer.ts
+++ src/utils/abstract-equlity-comparer.ts
@@ -1,13 +1,13 @@
 export abstract class AbstractEqualityComparer<T> {
     public abstract getHashCode(object: T) : string;
-    public abstract isEquals(first: T, seconds: T) : boolean;
+    public abstract equal(first: T, seconds: T) : boolean;
 }
 
-export class DefaultEqqualityComparer<T> extends AbstractEqualityComparer<T> {
+export class DefaultEqualityComparer<T> extends AbstractEqualityComparer<T> {
     public getHashCode(object: T): string {
         throw new Error("Method not implemented.");
     }
-    public isEquals(first: T, seconds: T): boolean {
+    public equal(first: T, seconds: T): boolean {
         return first === seconds;
     }
 }
